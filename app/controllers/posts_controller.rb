262c1ca1d3ecc6a4a97fc17dbfe8e4a38d7c94a5
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-08 13:34:18 -0400

    finished lab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,14 +23,14 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  post = Post.find(params[:id])
-	  post.update(post_params)
-	  redirect_to post_path(post)
+		post = Post.find(params[:id])
+		post.update(post_params)
+		redirect_to post_path(post)
 	end
 
-  private
+	private
 
-  def post_params
-    params.require(:post).permit(:title, :description)
-  end
+	def post_params
+		params.require(:post).permit(:title, :description)
+	end
 end
